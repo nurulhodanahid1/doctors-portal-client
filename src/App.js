fab09f9987d56f7f936032e57b27173e053afdcc
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-10-12 21:27:00 +0600

    home page done

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home/Home';
+import Appointment from './components/Appointment/Appointment/Appointment';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/appointment">
+          <Appointment></Appointment>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
